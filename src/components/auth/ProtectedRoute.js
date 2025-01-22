984cf00f91fe0075c72333631887e07ad06fa450
Felix Idalia <flxidalia@gmail.com>
2025-01-22 16:06:37 +0300

    Modified file

--- src/components/auth/ProtectedRoute.js
+++ src/components/auth/ProtectedRoute.js
@@ -1,3 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate, useLocation } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const user = useSelector((state) => state.auth.user);
+  const location = useLocation();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    // Prevent rendering on the server
+    return null;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
+
+{/*
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
@@ -14,4 +42,4 @@ const ProtectedRoute = ({ children }) => {
   return children;
 };
 
-export default ProtectedRoute;
+export default ProtectedRoute; */}
