3731e4ea35d20085a9369c1990e74e8e06adba60
Felix Idalia <flxidalia@gmail.com>
2025-01-22 16:17:41 +0300

    Modified: Added optional chaining

--- src/components/auth/ProtectedRoute.js
+++ src/components/auth/ProtectedRoute.js
@@ -1,29 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-  const user = useSelector((state) => state.auth.user);
+  // Add optional chaining to prevent undefined errors
+  const user = useSelector((state) => state?.auth?.user);
   const location = useLocation();
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    // Prevent rendering on the server
-    return null;
-  }
 
   if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
+    return <Navigate to="/login" state={{ from: location.pathname }} replace />;
   }
 
   return children;
 };
 
 export default ProtectedRoute;
+
 
 {/*
 import React from 'react';
